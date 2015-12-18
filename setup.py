ffda92b150b6198c8a0dc551d3b62697786e743f
minamorl <minamorl@users.noreply.github.com>
2015-12-18 19:01:45 +0900

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ appname = "epinephrine"
 
 setup(
     name = appname,
-    version = "0.0.3",
+    version = "0.0.4",
     packages = find_packages(),
     entry_points={
         'console_scripts': [
